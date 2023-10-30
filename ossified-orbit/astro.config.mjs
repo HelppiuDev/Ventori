54c04ac1d3a253e7426bcd5239c6846481cd73fc
HelppiuDev <bmesa.helppiu@gmail.com>
2023-10-30 11:00:07 -0500

    build package

--- ossified-orbit/astro.config.mjs
+++ ossified-orbit/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
-// import node from "@astrojs/node";
+ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
     //  root: './src/pages/landing.astro',
 
 // srcDir: './src/pages/ventori.astro',
-// outDir: './Ventori_Build',
-//   output: 'server',
-//   adapter: node({
-//     mode: "standalone"
-//   })
+ outDir: './Ventori_Build',
+   output: 'server',
+ adapter: node({
+   mode: "standalone"
+  })
 });
